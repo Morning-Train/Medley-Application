2f94604cff4aafa55b86e9283e83654698d9b7af
Mathias Munk <mh@morningtrain.dk>
2024-09-16 09:38:39 +0200

    Boot artisan

--- src/MorningMedley.php
+++ src/MorningMedley.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Contracts\Debug\ExceptionHandler as ExceptionHandlerContract;
+
 class MorningMedley
 {
     readonly public \MorningMedley\Application\Application $app;
@@ -12,12 +14,30 @@ class MorningMedley
         $this->app = new \MorningMedley\Application\Application($this->baseDir);
         $this->app->singleton(Illuminate\Contracts\Http\Kernel::class, \MorningMedley\Application\Http\Kernel::class);
 
-        $this->app->singleton(
-            Illuminate\Contracts\Debug\ExceptionHandler::class,
-            App\Exceptions\Handler::class
-        );
-
         $this->app->make(Illuminate\Contracts\Http\Kernel::class)->bootstrap();
+
+        if ($this->app->runningInConsole()) {
+            \WP_CLI::add_command('artisan', [$this, 'artisan']);
+        }
     }
 
+    public function artisan()
+    {
+        define('LARAVEL_START', microtime(true));
+
+        $this->app->bind(ExceptionHandlerContract::class,
+            \MorningMedley\Application\Console\ExceptionHandler::class);
+        $this->app->singleton(Illuminate\Contracts\Console\Kernel::class,
+            \MorningMedley\Application\Console\Kernel::class);
+        $kernel = $this->app->make(Illuminate\Contracts\Console\Kernel::class);
+
+        $argv = array_slice($_SERVER['argv'], 2); // Trim WordPress args
+
+        $status = $kernel->handle(
+            $input = new Symfony\Component\Console\Input\ArgvInput($argv),
+            new Symfony\Component\Console\Output\ConsoleOutput
+        );
+        $kernel->terminate($input, $status);
+        exit($status);
+    }
 }

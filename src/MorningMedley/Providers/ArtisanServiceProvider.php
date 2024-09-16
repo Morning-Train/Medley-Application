96162ec53594bb1c671804a905fc7f4891000810
Mathias Munk <mh@morningtrain.dk>
2024-09-16 10:52:15 +0200

    Add cache commands

--- src/MorningMedley/Providers/ArtisanServiceProvider.php
+++ src/MorningMedley/Providers/ArtisanServiceProvider.php
@@ -5,13 +5,21 @@ namespace MorningMedley\Application\Providers;
 use Illuminate\Console\Signals;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Foundation\Console\AboutCommand;
+
+use Illuminate\Foundation\Console\ConfigClearCommand;
+use Illuminate\Foundation\Console\ConfigPublishCommand;
+use Illuminate\Foundation\Console\ConfigShowCommand;
 use Illuminate\Foundation\Console\EnvironmentCommand;
 use Illuminate\Support\ServiceProvider;
+use MorningMedley\Application\Console\ConfigCacheCommand;
 
 class ArtisanServiceProvider extends ServiceProvider implements DeferrableProvider
 {
     protected $commands = [
         'About' => AboutCommand::class,
+        'ConfigCache' => ConfigCacheCommand::class,
+        'ConfigClear' => ConfigClearCommand::class,
+        'ConfigShow' => ConfigShowCommand::class,
         'Environment' => EnvironmentCommand::class
     ];
 
@@ -53,4 +61,24 @@ class ArtisanServiceProvider extends ServiceProvider implements DeferrableProvid
             return new AboutCommand($app['composer']);
         });
     }
+
+    protected function registerConfigCacheCommand()
+    {
+        $this->app->singleton(ConfigCacheCommand::class, function ($app) {
+            return new ConfigCacheCommand($app['files']);
+        });
+    }
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerConfigClearCommand()
+    {
+        $this->app->singleton(ConfigClearCommand::class, function ($app) {
+            return new ConfigClearCommand($app['files']);
+        });
+    }
+
 }

fafa14ccef2ec1e0ce241df9a65bf156992540d1
Mathias Munk <mh@morningtrain.dk>
2024-12-17 09:51:51 +0100

    Remove unused imports

--- src/MorningMedley/Providers/WpContextServiceProvider.php
+++ src/MorningMedley/Providers/WpContextServiceProvider.php
@@ -2,18 +2,10 @@
 
 namespace MorningMedley\Application\Providers;
 
-use Illuminate\Console\Signals;
-use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Support\DeferrableProvider;
-use Illuminate\Foundation\Console\AboutCommand;
 
-use Illuminate\Foundation\Console\ConfigClearCommand;
 use Illuminate\Foundation\Console\ConfigPublishCommand;
-use Illuminate\Foundation\Console\ConfigShowCommand;
-use Illuminate\Foundation\Console\EnvironmentCommand;
-use Illuminate\Foundation\Console\PackageDiscoverCommand;
 use Illuminate\Support\ServiceProvider;
-use MorningMedley\Application\Console\ConfigCacheCommand;
 use MorningMedley\Application\WpContext\PluginContext;
 use MorningMedley\Application\WpContext\ThemeContext;
 use MorningMedley\Application\WpContext\WpContextContract;

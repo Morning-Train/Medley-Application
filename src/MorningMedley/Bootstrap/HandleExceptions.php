d07f33d0c957f8de2cb112a0a46ba260c92f4e2d
Mathias Munk <mh@morningtrain.dk>
2025-01-29 16:31:54 +0100

    Refining exception handling and output

--- src/MorningMedley/Bootstrap/HandleExceptions.php
+++ src/MorningMedley/Bootstrap/HandleExceptions.php
@@ -7,6 +7,69 @@ use Throwable;
 
 class HandleExceptions extends \Illuminate\Foundation\Bootstrap\HandleExceptions
 {
+    protected $defaultExceptionHandler = null;
+
+    /**
+     * Bootstrap the given application.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     * @return void
+     */
+    public function bootstrap(Application $app)
+    {
+        static::$reservedMemory = str_repeat('x', 32768);
+
+        static::$app = $app;
+
+        error_reporting(-1);
+
+        set_error_handler($this->forwardsTo('handleError'));
+
+        set_exception_handler($this->forwardsTo('handleException'));
+
+        register_shutdown_function($this->forwardsTo('handleShutdown'));
+    }
+
+    /**
+     * Handle an uncaught exception from the application.
+     *
+     * Note: Most exceptions can be handled via the try / catch block in
+     * the HTTP and Console kernels. But, fatal error exceptions must
+     * be handled differently since they are not normal exceptions.
+     *
+     * @param  \Throwable  $e
+     * @return void
+     */
+    public function hssandleException(Throwable $e)
+    {
+        static::$reservedMemory = null;
+
+        try {
+            $this->getExceptionHandler()->report($e);
+        } catch (\Exception) {
+            $exceptionHandlerFailed = true;
+        }
+        if (! config('app.debug')) {
+            try {
+                $this->defaultExceptionHandler(...func_get_args()); // Try to fall back if possible
+            } catch (\Throwable $e) {
+
+            }
+
+            return;
+        }
+
+        if (static::$app->runningInConsole()) {
+            $this->renderForConsole($e);
+
+            if ($exceptionHandlerFailed ?? false) {
+                exit(1);
+            }
+        } else {
+            $this->renderHttpResponse($e);
+        }
+    }
+
     /**
      * Render an exception as an HTTP response and send it.
      *
